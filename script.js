43ee9ac71e34614e08c155f7774c621d192811dc
Shruti-229411 <misalpav0108@gmail.com>
2026-04-28 21:20:45 +0530

    feat: all html pages, css and script

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if(hamburger) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
